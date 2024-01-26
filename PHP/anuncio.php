586d69b986f04893a269fd7d95522f72a6702855
Guilherme-Santt <guisant@icloud.com>
2024-01-26 20:09:05 -0300

    correções tabble

--- PHP/anuncio.php
+++ PHP/anuncio.php
@@ -27,23 +27,24 @@
     $infos = $query_infos->fetch_assoc();
     var_dump($infos);
 
-if( $contagem == 0){ ?>
+    if( $contagem == 0){ ?>
         <tr>
             <td colspan="6">Nenhum anúncio encontrado</td>
         <tr>
-    <?php }else{
+    <?php }
+    else{ 
         while($infos = $query_infos->fetch_assoc()) { ?>  
         <tr>
-            <td><img class="img_tbody" href="arquivos/<?php $query_infos['path'];?>"></td>
-            <td><?php $info['titulo'];  ?></td>
-            <td><?php $info['preco'];  ?></td>
+            <td><img class="img_tbody" src="arquivos/<?php $infos['path']; ?>"></td>
+            <td><?php $infos['preco'];  ?></td>
             <td><?php $info['modelo']; ?> </td>
             <td><?php $info['marca'];  ?></td>
             <td><?php $info['data_compra'];?></td>
             <td><?php $info['km'];?></td>
         </tr>
-    <?php } 
-            }?> 
+    <?php
+        } 
+    }?> 
     </tbody>
 </table>
 </body>
